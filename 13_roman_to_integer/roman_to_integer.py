c52c6adf18e1e3528fc2b84fe45bc4fbdaa0ea1f
kdzumba <knowledgedzumba@gmail.com>
2025-01-29 22:44:05 +0200

    roman to integer

---
+++ 13_roman_to_integer/roman_to_integer.py
@@ -0,0 +1,67 @@
+'''
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. 
+Instead, the number four is written as IV. Because the one is before the five we subtract it making four. 
+The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+
+Given a roman numeral, convert it to an integer.
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ 
+
+Constraints:
+
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+'''
+
+class Solution:
+    BASE_VALUES = {
+            "I": 1, 
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+            "IV": 4,
+            "IX": 9,
+            "XL": 40,
+            "XC": 90,
+            "CD": 400,
+            "CM": 900
+        }
+    
+    def roman_to_int(self, s: str) -> int:
+        n = len(s)
+        denominations = []
+
+        cursor = 0
+
+        while cursor < n:
+            # Taking advantage of string slicing's forgiving nature for IndexError, won't fail
+            pair = s[cursor:cursor + 2] 
+            if pair in self.BASE_VALUES:
+                denominations.append(self.BASE_VALUES[pair])
+                cursor += 2
+            else:
+                denominations.append(self.BASE_VALUES[s[cursor]])
+                cursor += 1
+        return sum(denominations)
